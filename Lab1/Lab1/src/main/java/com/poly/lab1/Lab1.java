7659463c818262d08ee766746869c21fc570c2f5
hoangduy2608 <hahoangduy268@gmail.com>
2026-09-18 18:36:40 +0700

    Hoan Thanh Lab1 Bai1

---
+++ Lab1/Lab1/src/main/java/com/poly/lab1/Lab1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.poly.lab1;
+
+/**
+ *
+ * @author DELL
+ */
+public class Lab1 {
+    public static void main(String[] args) {
+        System.out.println("Ho ten: Ha Hoang Duy, MSSV: PS50175");
+    }
+}
