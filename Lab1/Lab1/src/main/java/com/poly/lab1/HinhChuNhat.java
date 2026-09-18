0f8a6422a85dab289d2dc2fc823a6c79d04bbf3d
hoangduy2608 <hahoangduy268@gmail.com>
2026-09-18 19:05:35 +0700

    Hoan Thanh Bai4 Lab1

---
+++ Lab1/Lab1/src/main/java/com/poly/lab1/HinhChuNhat.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poly.lab1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DELL
+ */
+public class HinhChuNhat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double cd, cr, cv , dt;
+        
+        System.out.print("Nhap chieu dai: ");
+        cd = sc.nextDouble();
+        System.out.print("Nhap chieu rong: ");
+        cr = sc.nextDouble();
+        
+        cv = 2 * (cd + cr);
+        dt = cd * cr;
+        
+        System.out.printf("Chieu dai: %.2f\nChieu rong: %.2f\nChu vi: %.2f\nDien tich: %.2f\n", cd, cr, cv, dt);
+    }
+}
